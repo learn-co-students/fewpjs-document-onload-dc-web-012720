7026ee949197a83d18c8e4ac143cd1104f20c374
matteoaricci <matteo.a.ricci@gmail.com>
2020-03-09 10:04:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   
+    rewriteText()
+    
+});
+
+function rewriteText() {
+
+    let textSection = document.getElementById("text")
+    
+    textSection.textContent = "This is really cool!"
+}
